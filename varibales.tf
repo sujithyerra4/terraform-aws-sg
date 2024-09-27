adecf418501173a1b75547aa6cb5d446ac023dc8
sujithyerra4 <sujithyerra483@gmail.com>
2024-09-27 20:10:35 +0530

    terraform

---
+++ varibales.tf
@@ -0,0 +1,23 @@
+variable "project_name"{
+
+}
+
+variable "environment"{
+
+}
+
+variable "sg_name" {
+
+}
+
+variable "vpc_id" {
+
+}
+
+variable "common_tags"{
+    default = {}
+}
+
+variable "sg_tags"{
+default={}
+}
